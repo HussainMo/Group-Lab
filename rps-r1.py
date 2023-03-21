18b7f98eb8a6643445a884326ba44f379072f373
HussainMo <mohammadihu91@gmail.com>
2023-03-21 02:29:16 -0400

    Adds rps-r1.py

---
+++ rps-r1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import random
+
+
+'''
+This is an implementation of the classic "Rock, Paper, Scissors" game in Python. 
+The game developed in single player mode and the user should competes against the computer. 
+The app asks a user to enter their desired choice among rock, paper, or scissors. Then
+randomly chooses an option for the computer too. After that, the app determines the winner
+based on the game's rules.
+This app doesn't offer any graphical interface or complex features.
+
+Date: March 22, 2023
+'''
+
+
+options = ("rock", "paper", "scissors") # list of options for the computer
+computer = random.choice(options) # This will choose a random option from your list for the computer
